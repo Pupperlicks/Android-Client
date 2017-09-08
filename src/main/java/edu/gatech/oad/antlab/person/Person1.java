0a2d6fff351279a5916e836bfeb0c37d901acf44
Warren Waleed <warrenwaleed8@gmail.com>
2017-09-08 12:33:38 -0400

    Implemented M2 Personal Java modifications

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -30,8 +30,13 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+        String yo = "";
+        for(int i = 2; i < input.length(); i++) {
+            yo += input.charAt(i);
+        }
+        yo += input.charAt(0);
+        yo += input.charAt(1);
+	  return yo;
 	}
 	
 	/**
