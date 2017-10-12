7d00df490423612b92701b8d2a95c2fd716cd3a6
Cooper Pellaton <cooper@cooperpellaton.com>
2017-10-12 01:04:42 -0400

    M6 Baseline

---
+++ app/src/main/java/com/example/cooperpellaton/pupperlicks/RatSighting.java
@@ -0,0 +1,71 @@
+package com.example.cooperpellaton.pupperlicks;
+
+/**
+ * Created by Cooper Pellaton on 10/11/2017.
+ * The representation of a rat object so that we can use Firebase UI.
+ */
+
+public class RatSighting {
+    private String uniqueKey;
+    private String createdDate;
+    private String closedDate;
+    private String agency;
+    private String agencyName;
+    private String complaint;
+    private String complaintType;
+    private String descriptor;
+    private String locationType;
+    private String incidentZip;
+    private String incidentAddress;
+    private String streetName;
+    private String crossStreet1;
+    private String crossStreet2;
+    private String intersectionStreet1;
+    private String intersectionStreet2;
+    private String addressType;
+    private String city;
+    private String landmark;
+    private String facilityType;
+    private String status;
+    private String dueDate;
+    private String resolutionActionUpdatedDate;
+    private String communityBoard;
+    private String borough;
+    private String xCoordinateStatePlane;
+    private String yCoordinateStatePlane;
+    private String parkFacilityName;
+    private String parkBorough;
+    private String schoolName;
+    private String schoolNumber;
+    private String schoolRegion;
+    private String schoolCode;
+    private String schoolPhoneNumber;
+    private String schoolAddress;
+    private String schoolCity;
+    private String schoolState;
+    private String schoolZip;
+    private String schoolNotFound;
+    private String schoolOrCityWideComplaint;
+    private String vehicleType;
+    private String taxiCompanyBorough;
+    private String taxiPickupLocation;
+    private String bridgeHighwayLocation;
+    private String bridgeHighwayName;
+    private String bridgeHighwayDirection;
+    private String roadRamp;
+    private String bridgeHighwaySegment;
+    private String garageLotName;
+    private String ferryDirection;
+    private String ferryTerminalName;
+    private String latitude;
+    private String longitude;
+    private String location;
+
+    public RatSighting(){
+
+    }
+
+
+
+
+}
