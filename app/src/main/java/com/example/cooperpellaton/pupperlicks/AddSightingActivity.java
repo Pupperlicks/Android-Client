836c6351320d30cb748d2781a88afdb16e332f91
vjoh <vljoh05@wildblue.net>
2017-10-25 16:05:15 -0400

    Fixed the implementation of onClick(View) as opposed to OnClick(View); there was a typo

--- app/src/main/java/com/example/cooperpellaton/pupperlicks/AddSightingActivity.java
+++ app/src/main/java/com/example/cooperpellaton/pupperlicks/AddSightingActivity.java
@@ -3,6 +3,7 @@ package com.example.cooperpellaton.pupperlicks;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -34,9 +35,10 @@ public class AddSightingActivity extends Activity {
         // set up button
         btnSave = (Button) findViewById(R.id.button2);
 
-        btnSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void OnClick(View v) {
+        btnSave.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v) {
+
                 ServerPortal sp = new ServerPortal();
                 // build rat sighting object and send server add request
                 sp.addReport(new RatSighting(
