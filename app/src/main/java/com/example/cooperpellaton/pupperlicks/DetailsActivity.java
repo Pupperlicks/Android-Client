e4579643fb4bf8cd3509542801dfa52501d3674f
Blane Legant <blane.legant@gmail.com>
2017-10-31 00:41:43 -0400

    completely revamped DetailsActivity: app bar and much easier to read

--- app/src/main/java/com/example/cooperpellaton/pupperlicks/DetailsActivity.java
+++ app/src/main/java/com/example/cooperpellaton/pupperlicks/DetailsActivity.java
@@ -1,8 +1,10 @@
 package com.example.cooperpellaton.pupperlicks;
 
-import android.content.Intent;
+import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.widget.TextView;
 
 /**
@@ -19,23 +21,40 @@ public class DetailsActivity extends AppCompatActivity {
         Bundle b = getIntent().getExtras();
         RatSighting sighting;
 
-        TextView detailsView = findViewById(R.id.details_text_view);
+        AppBarLayout appBarLayout = findViewById(R.id.appbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        // get references to the TextViews in the layout
+        TextView tvCreatedDate = findViewById(R.id.textViewCreatedDate);
+        TextView tvLocationType = findViewById(R.id.textViewLocationType);
+        TextView tvIncidentZip = findViewById(R.id.textViewZip);
+        TextView tvIncidentAddress = findViewById(R.id.textViewAddress);
+        TextView tvCity = findViewById(R.id.textViewCity);
+        TextView tvBorough = findViewById(R.id.textViewBorough);
+        TextView tvCoords = findViewById(R.id.textViewCoords);
+
+//        TextView detailsView = findViewById(R.id.details_text_view);
 
         if (b != null) {
             // retrieve the sighting from the bundle
             sighting = (RatSighting) b.getSerializable("details"); // de-serialize the object
-            detailsView.setText("Unique Key: " + sighting.getUniqueKey()
-                    + "\nCreated Date: " + sighting.getCreatedDate()
-                    + "\nLocation Type: " + sighting.getLocationType()
-                    + "\nIncident Zip: " + sighting.getIncidentZip()
-                    + "\nIncident Address: " + sighting.getIncidentAddress()
-                    + "\nCity: " + sighting.getCity()
-                    + "\nBorough: " + sighting.getBorough()
-                    + "\nLatitude: " + sighting.getLatitude()
-                    + "\nLongitude: " + sighting.getLongitude()
-            );
+
+            tvCreatedDate.setText(sighting.getCreatedDate());
+            tvLocationType.setText(sighting.getLocationType());
+            tvIncidentZip.setText(sighting.getIncidentZip());
+            tvIncidentAddress.setText(sighting.getIncidentAddress());
+            tvCity.setText(sighting.getCity());
+            tvBorough.setText(sighting.getBorough());
+            tvCoords.setText(sighting.getLatitude() + ", " + sighting.getLongitude());
+
+            this.getSupportActionBar().setTitle("Sighting " + sighting.getUniqueKey());
+
         } else {
-            detailsView.setText("Sighting not Found.");
+//            detailsView.setText("Sighting not Found.");
+            Log.e("error", "A RatSIghting object wasn't passed in the bundle.");
         }
     }
 }
