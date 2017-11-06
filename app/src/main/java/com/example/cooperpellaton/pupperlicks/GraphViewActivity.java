2e21468468096d74dab73124df7b1ecfafa42a74
vjoh <vljoh05@wildblue.net>
2017-11-06 09:55:43 -0500

    Process of implementating GraphViewActivity and activity_graph

--- app/src/main/java/com/example/cooperpellaton/pupperlicks/GraphViewActivity.java
+++ app/src/main/java/com/example/cooperpellaton/pupperlicks/GraphViewActivity.java
@@ -1,5 +1,6 @@
 package com.example.cooperpellaton.pupperlicks;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
@@ -9,8 +10,10 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Adapter;
+import android.widget.DatePicker;
 import android.widget.TextView;
 import android.view.*;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 
@@ -18,7 +21,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import com.jjoe64.graphview.*;
@@ -33,7 +39,17 @@ import com.jjoe64.graphview.series.LineGraphSeries;
  *
  */
 
-public class GraphViewActivity extends AppCompatActivity {
+public class GraphViewActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    // sightings date range selection stuff
+    private DatePickerDialog endDatePickerDialog; // UI
+    private DatePickerDialog startDatePickerDialog; // UI
+    private DatePicker startDatePicker;
+    private DatePicker endDatePicker;
+    private Date startDate;
+    private Date endDate;
+
+    Context context;
 
     public GraphViewActivity(ArrayList<RatSighting> ratSightings) {
 
@@ -59,6 +75,19 @@ public class GraphViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_graph);
         Bundle b = getIntent().getExtras();
         this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+
+        // set up Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar6);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // enable back button
+        getSupportActionBar().setTitle("Rat Sightings Graph");
+
+        // set context var
+        context = getApplicationContext();
+
     }
 
 
@@ -67,6 +96,74 @@ public class GraphViewActivity extends AppCompatActivity {
      return true;
     }
 
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        // the "correct" way to set a date object (old constructor was deprecated)
+        Calendar cal = Calendar.getInstance(); // create calendar object
+        cal.set(Calendar.YEAR, year); // set year
+        cal.set(Calendar.MONTH, month); // set month (starting at Jan = 0)
+        cal.set(Calendar.DAY_OF_MONTH, day); // set day
+        Date date = cal.getTime(); // convert to Date object
+
+        // set up dateformatter for debug purposes
+        SimpleDateFormat df = new SimpleDateFormat("M/d/yyyy H:mm");
+
+        // determine which date picker was just used: the one for the starting date, or the one
+        // for the ending date?
+        if(datePicker == startDatePicker) {
+            startDate = date; // set the instance variable
+            // place date in debug log
+            Log.d("PupperLicks/DatePicker", "startDate: " + df.format(startDate));
+
+
+            // now that we have the start date, get the end date
+            endDatePickerDialog = new DatePickerDialog(GraphViewActivity.this, GraphViewActivity.this, 2017, 0, 1);
+            // store the reference to the internal DatePicker so we can determine which one was pressed later
+            endDatePickerDialog.setTitle("End Date");
+            endDatePicker = endDatePickerDialog.getDatePicker();
+            endDatePickerDialog.show();
+            // display a helpful toast indicating what we need to be selecting
+            Toast.makeText(context, "Select end date", Toast.LENGTH_LONG).show();
+
+        } else if (datePicker == endDatePicker) {
+            endDate = date; // set the instance variable
+            // place date in debug log
+            Log.d("PupperLicks/DatePicker", "endDate: " + df.format(endDate));
+
+            // TODO: this is where the item retrieval/filtering needs to be done
+
+            // this should never happen (famous last words...)
+        } else {
+            throw new IllegalArgumentException("Invalid View passed, cannot parse date.");
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.actions_maps_datefilter) {
+
+            // get the start date
+            startDatePickerDialog = new DatePickerDialog(GraphViewActivity.this, GraphViewActivity.this, 2015, 0, 1);
+
+            // store the reference to the internal DatePicker so we can determine which one was pressed later
+            startDatePicker = startDatePickerDialog.getDatePicker();
+            startDatePickerDialog.show();
+
+            // display a helpful toast indicating what we need to be selecting
+            Toast.makeText(context, "Select start date", Toast.LENGTH_LONG).show();
+
+            return true; // consume the click event
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
 
 }
