acbd33042655d529cee828bf2696a015ad04b248
vjoh <vljoh05@wildblue.net>
2017-11-14 11:42:18 -0500

    Added a jUnit to test whether or not DetailsActivity.getSighting() populated a RatSighting object.

--- app/src/test/java/com/example/cooperpellaton/pupperlicks/GetSightingTest.java
+++ app/src/test/java/com/example/cooperpellaton/pupperlicks/GetSightingTest.java
@@ -1,18 +1,17 @@
 package com.example.cooperpellaton.pupperlicks;
 
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 /**
  *
  * To test if DetailsActivity.getSighting() populates a RatSighting.
  * Created by Victoria Joh on 11/13/17.
  */
-
-
-
 
 public class GetSightingTest {
 
@@ -22,10 +21,34 @@ public class GetSightingTest {
            "incidentZip", "incidentAddress", "city","borough",
            "latitude", "longitude");
 
+
+    /**
+     * Test whether a Rat Sighting has been created.
+     */
     @Test
     public void sightingIsPopulated() {
 
-
-        assertEquals(sighting, activity.getSighting());
+        assertFalse(sighting.equals(activity.getSighting()));
     }
+
+    /**
+     * Test whether the good values are fetched from the Rat Sighting properly.
+     */
+    @Test
+    public void properValuesTest() {
+        assertEquals("alolo", sighting.getUniqueKey());
+        assertEquals("alolo", sighting.getUniqueKey());
+        assertEquals("11/13/2017", sighting.getCreatedDate());
+        assertEquals("locationType", sighting.getLocationType());
+        assertEquals("incidentZip", sighting.getIncidentZip());
+        assertEquals("city", sighting.getCity());
+        assertEquals("borough", sighting.getBorough());
+        assertEquals("latitude", sighting.getLatitude());
+        assertEquals("longitude", sighting.getLongitude());
+    }
+
+
+
+
+
 }
